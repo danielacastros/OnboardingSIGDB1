bb5ee02f88b88a34f9cb3a0f84a8bdb1dec6421b
daniela.soares <daniela.soares@db1.com.br>
2025-05-16 17:13:44 -0300

    Primeiro commit Data

--- OnboardingSIGDB1.Data/obj/Debug/net5.0/OnboardingSIGDB1.Data.AssemblyInfo.cs
+++ OnboardingSIGDB1.Data/obj/Debug/net5.0/OnboardingSIGDB1.Data.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Onboardingsig.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("OnboardingSIGDB1.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe47c26c413115612c108252146f8133a784818f")]
-[assembly: System.Reflection.AssemblyProductAttribute("Onboardingsig.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Onboardingsig.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("OnboardingSIGDB1.Data")]
+[assembly: System.Reflection.AssemblyTitleAttribute("OnboardingSIGDB1.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
